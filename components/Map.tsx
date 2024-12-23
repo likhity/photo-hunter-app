171ea85eaa9b107d1ee3acdb52eb4de8fd3d3ebe
likhity <73198499+likhity@users.noreply.github.com>
2024-12-22 23:33:11 -0700

    Render markers on map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,12 +1,37 @@
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, {
+  Camera,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+
+import pin from '~/assets/photohunt-icon.png';
+import photohunts from '~/data/photohunts.json';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN || '');
 
 export default function Map() {
+  const points = photohunts.map((photohunt) => point([photohunt.long, photohunt.lat]));
+
   return (
-    <MapView style={{ flex: 1 }}>
+    <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
       <Camera followUserLocation followZoomLevel={15} />
       <LocationPuck puckBearing="heading" puckBearingEnabled pulsing={{ isEnabled: true }} />
+
+      <ShapeSource id="photohunts" shape={featureCollection(points)}>
+        <SymbolLayer
+          id="photohunter-icons"
+          style={{
+            iconImage: 'pin',
+            iconSize: 0.3,
+            iconAllowOverlap: true,
+          }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 }
